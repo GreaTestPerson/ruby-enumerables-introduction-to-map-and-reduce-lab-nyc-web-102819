6a249444720daaf6cdd5ddde1f2309789149042d
Lilibeth Santo <itzlilibeth@gmail.com>
2019-10-26 17:35:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -72,9 +72,12 @@ def reduce_to_all_true(source_array)
 end 
 
 def reduce_to_any_true(source_array)
-  if source_array.include?true
-    return true
-  else
-    return false
+ i = 0 
+  while i < source_array.length do 
+    if (source_array[i] == true)
+     return true
   end
+      i += 1
+  end
+   false
 end
